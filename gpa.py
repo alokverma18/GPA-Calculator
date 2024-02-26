3c063a0758f23c9ddfd053bad56684c95e45cdfc
Alok Verma <as1492@srmist.edu.in>
2024-02-26 22:41:55 +0530

    Update gpa.py
    Signed-off-by: Alok Verma <as1492@srmist.edu.in>

--- gpa.py
+++ gpa.py
@@ -57,9 +57,10 @@ if st.button("Calculate GPA"):
 # Footer
 col = st.columns(4)
 
+'''
 with col[3]:
     st.write("Made with ❤ by [Alok](https://github.com/alokverma18/gpa-calculator)")
-
+'''
 
 
 
